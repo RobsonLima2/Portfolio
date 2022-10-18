f2b75477ad651d8ad6365fcb3aa16a2c6bdec46e
RobsonLima2 <robson.ramos.lima@hotmail.com>
2022-10-17 21:15:37 -0300

    finalização da aba works

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,11 +1,49 @@
 import React from "react";
-import * as S from "../styles/styledHome";
+import * as S from "../styles/styledWorks.js";
+import Recipes from "../assets/img/Recipes.jpg";
+import Lol from "../assets/img/LOL.jpg";
+import Portflow from "../assets/img/portflow.jpg";
+import Finn from "../assets/img/FINN.jpg";
 
 function Works() {
   return (
-    <div>
-      <h2>Woks</h2>
-    </div>
+    <S.ContainerW>
+      <a href="https://recipes-vnw.vercel.app/">
+        <S.BoxOne>
+          <S.ImgBox src={Recipes} alt="" />
+          <S.TitleBox>
+            <S.H2Box>Recipes</S.H2Box>
+          </S.TitleBox>
+        </S.BoxOne>
+      </a>
+
+      <a href="https://robsonlima2.github.io/Challenge_LOL/">
+        <S.BoxOne>
+          <S.ImgBox src={Lol} alt="" />
+          <S.TitleBox>
+            <S.H2Box>LOL</S.H2Box>
+          </S.TitleBox>
+        </S.BoxOne>
+      </a>
+
+      <a href="https://robsonlima2.github.io/Portflow/">
+        <S.BoxOne>
+          <S.ImgBox src={Portflow} alt="" />
+          <S.TitleBox>
+            <S.H2Box>Portflow</S.H2Box>
+          </S.TitleBox>
+        </S.BoxOne>
+      </a>
+
+      <a href="https://robsonlima2.github.io/Finn/">
+        <S.BoxOne>
+          <S.ImgBox src={Finn} alt="" />
+          <S.TitleBox>
+            <S.H2Box>Finn</S.H2Box>
+          </S.TitleBox>
+        </S.BoxOne>
+      </a>
+    </S.ContainerW>
   );
 }
 
---
+++ src/styles/styledWorks.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const ContainerW = styled.section`
+  width: 95%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  @media (max-width: 950px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const BoxOne = styled.div`
+  width: 300px;
+  height: 250px;
+  margin: 8vh 0 0vh 0;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+  margin-left: 20px;
+  border-radius: 15%;
+`;
+
+export const ImgBox = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const TitleBox = styled.div`
+  width: 100%;
+  height: 8vh;
+  background-image: linear-gradient(180deg, transparent 0%, #000 100%);
+  z-index: 5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
+export const H2Box = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-weight: bold;
+  font-weight: 900;
+  color: #fffaf0;
+`;
